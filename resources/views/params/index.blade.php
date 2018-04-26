0993a59e770601c92ddae9d3abfc90f0ed242b44
mcbnn <mc_bnn@mail.ru>
2018-04-26 12:38:52 +0300

    route

--- resources/views/params/index.blade.php
+++ resources/views/params/index.blade.php
@@ -31,7 +31,7 @@
                                             </a>
                                         </td>
                                         <td>
-                                            {{ Str::limit($item->name, 10) }}
+                                            {{ str_limit($item->name, $limit = 150, $end = '...') }}
                                         </td>
                                         <td>
                                             <a target="_blank" href="{{$item->value}}">
