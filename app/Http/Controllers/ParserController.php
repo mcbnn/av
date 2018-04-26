2a02a86ddb926c3d4f350c8a235f5852082a4fd9
mcbnn <mc_bnn@mail.ru>
2018-04-26 12:43:30 +0300

    route

--- app/Http/Controllers/ParserController.php
+++ app/Http/Controllers/ParserController.php
@@ -31,6 +31,7 @@ class ParserController extends Controller
         $param = \App\Params::find($id);
         if(!$param)abort(404);
         $urls = $this->getPaginationLink($param->value);
+        var_dump($urls);die();
         $parsers = $this->parser($urls);
         $param->saveContents($parsers);
         return redirect('params');
