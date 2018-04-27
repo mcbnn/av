7873ae683f56baecd45bd1a48576189c5e4e20f1
mcbnn <mc_bnn@mail.ru>
2018-04-27 12:38:31 +0300

    route

--- app/Params.php
+++ app/Params.php
@@ -48,8 +48,9 @@ class Params extends Model
         if(!$parsers)return null;
         syslog(LOG_NOTICE,  json_encode($parsers));
         foreach($parsers as $key => $parser){
-            syslog(LOG_NOTICE, time().'--'.\App\Contents::where('key', $key)->orWhere('url', $parser)->count());
-            if(\App\Contents::where('key', $key)->orWhere('url', $parser)->count())continue;
+            $check = \App\Contents::where('key', $key)->orWhere('url', $parser)->count();
+            syslog(LOG_NOTICE,  $key);
+            if($check)continue;
             $content = new \App\Contents();
             $content->key = $key;
             $content->url = $parser;
