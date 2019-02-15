3f85681967cc9be325ca10dafd63a0d8ce3c4472
Николай Бодров <bodrov@simicon.com>
2019-02-15 15:36:21 +0300

    clear

--- app/Http/Controllers/ParserController.php
+++ app/Http/Controllers/ParserController.php
@@ -124,6 +124,7 @@ class ParserController extends Controller
     {
         $html = $this->getHtmlAvito($url);
 	    if(!$html)return;
+	    var_dump($html);die();
         $dom = HtmlDomParser::str_get_html($html);
         $_url = $dom->find('a.pagination-page');
         if(!count($_url))return [$url];
